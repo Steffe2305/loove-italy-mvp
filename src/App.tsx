0d36ae2b24fdc88ec52a18b88c6c46a047e2f32f
steffe2305 <stefano@skywaysviaggi.it>
2026-05-22 18:31:01 +0200

    Fix unused useMemo import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import { Routes, Route, useNavigate } from "react-router-dom"
 
 import { loadLooveHotels, type LooveHotel } from "./lib/looveData"
